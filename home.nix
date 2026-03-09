5d619fa49e6a458dec37d585548519ac30cdce48
Ricardho gunawan <98724584+RicardhoGunawan@users.noreply.github.com>
2026-03-09 12:42:47 +0700

    all mise

--- home.nix
+++ home.nix
@@ -2,14 +2,11 @@
 
 {
   home.stateVersion = "25.11";
-
   home.username = "ciiruu";
   home.homeDirectory = lib.mkForce "/Users/ciiruu";
 
   home.packages = with pkgs; [
-    # Nix hanya bertugas menginstal binary aplikasinya saja
-    php83
-    php83Packages.composer 
+    # PHP dan Composer dihapus dari sini agar tidak bentrok dengan mise
     pkg-config
     coreutils
   ];
@@ -21,18 +18,14 @@
     settings.show_program_path = true;
   };
 
-  # Konfigurasi Mise yang minimalis
   programs.mise = {
     enable = true;
     enableFishIntegration = true;
-    # globalConfig dihapus agar file ~/.config/mise/config.toml 
-    # tidak dikelola oleh Nix (menjadi writable)
   };
 
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      # Tetap aktifkan mise agar PATH otomatis terupdate saat buka terminal
       ${pkgs.mise}/bin/mise activate fish | source
     '';
   };
