4388736cb3ff57496956ac97d2ebfa86ced6db2d
Ricardho gunawan <98724584+RicardhoGunawan@users.noreply.github.com>
2026-04-09 09:35:37 +0700

    Change username and clean up home.nix
    Updated username and removed unnecessary programs from configuration.

--- home.nix
+++ home.nix
@@ -2,13 +2,11 @@
 
 {
   home.stateVersion = "25.11";
-  home.username = "ciiruu";
+
+  home.username = "indra";
   home.homeDirectory = lib.mkForce "/Users/ciiruu";
 
   home.packages = with pkgs; [
-    # PHP dan Composer dihapus dari sini agar tidak bentrok dengan mise
-    pkg-config
-    coreutils
   ];
 
   programs.home-manager.enable = true;
@@ -17,26 +15,4 @@
     enable = true;
     settings.show_program_path = true;
   };
-
-  programs.mise = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      # 1. Inisialisasi Homebrew (WAJIB PERTAMA)
-      if test -d /opt/homebrew/bin
-        /opt/homebrew/bin/brew shellenv | source
-      end
-
-      # 2. Tambahkan PHP ke PATH secara eksplisit (Pake -p agar jadi prioritas)
-      fish_add_path -p /opt/homebrew/opt/php@8.3/bin
-      fish_add_path -p /opt/homebrew/opt/php@8.3/sbin
-
-      # 3. Baru aktifkan mise
-      ${pkgs.mise}/bin/mise activate fish | source
-    '';
-   };
 }
