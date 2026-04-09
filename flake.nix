206c2617144216cfab447fb158f9da4273601870
Ricardho gunawan <98724584+RicardhoGunawan@users.noreply.github.com>
2026-04-09 09:34:51 +0700

    Refactor flake.nix for darwin system configuration

--- flake.nix
+++ flake.nix
@@ -1,117 +1,107 @@
 {
-  description = "Ciiruu macOS system configuration";
+  description = "Ciiruu darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Optional: Declarative tap management
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
     };
-
     homebrew-cask = {
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
-
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
       flake = false;
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
+  outputs = inputs@{
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+    ...
+  }:
   let
-    system = "aarch64-darwin";
-
-    configuration = { pkgs, ... }: {
-
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      # Search available package. visit https://search.nixos.org/packages
+      # $ nix search nixpkgs pkg_name
       nixpkgs.config.allowUnfree = true;
-
       environment.systemPackages = with pkgs; [
-
-        # basic cli
         coreutils
         curl
+        mkalias
+        stow
+        vim
         wget
         jq
         yq
-
-        # utilities
-        git
-        vim
-        stow
-
-        # dev tools
-        mise
-        act
-        cloudflared
       ];
 
       fonts.packages = with pkgs; [
         jetbrains-mono
-        powerline-symbols
         openmoji-color
+        powerline-symbols
       ];
 
       homebrew = {
         enable = true;
-
         onActivation = {
+          cleanup = "zap";
           autoUpdate = true;
           upgrade = true;
-          cleanup = "zap";
         };
-
         brews = [
+          "mise"
+          "mkcert"
+          # Compiling PHP
           "autoconf"
+          "re2c"
           "bison"
           "pkg-config"
-          "re2c"
-          "php@8.3"
-          "composer"
+          "libiconv"
+          "gd"
+          "gmp"
+          "libsodium"
+          "libpq"
+          "readline"
+          "gettext"
+          "bzip2"
+          "curl"
+          "libffi"
+          "libxml2"
+          "libxslt"
+          "zlib"
+          "icu4c"
+          "oniguruma"
+          "libzip"
         ];
-
         casks = [
-
-          # browsers
-          "google-chrome"
-
-          # development
-          "visual-studio-code"
-          "docker-desktop"
-          "postman"
-          "datagrip"
-	  "android-studio"
-	  "mysqlworkbench"
-
-          # networking
-          "cloudflare-warp"
-          "ngrok"
-          "openvpn-connect"
-
-          # utilities
-          "the-unarchiver"
-          "vlc"
-
-          # communication
-          "whatsapp"
-
-          # apps you already use
           "antigravity"
+          "docker-desktop"
+          "google-chrome"
+          "postman"
+          "the-unarchiver"
+          "visual-studio-code"
+          "whatsapp"
+          "ngrok"
         ];
-      };
 
       system.primaryUser = "ciiruu";
 
-      system.defaults = {
 
+
+      system.defaults = {
         dock = {
           autohide = true;
           mru-spaces = false;
@@ -124,57 +114,53 @@
             "/Applications/WhatsApp.app"
           ];
         };
-
-        NSGlobalDomain = {
-          AppleInterfaceStyle = "Dark";
-          AppleICUForce24HourTime = true;
-          AppleShowAllExtensions = true;
-          KeyRepeat = 2;
-          InitialKeyRepeat = 15;
-          ApplePressAndHoldEnabled = false;
-        };
-
-        finder = {
-          FXPreferredViewStyle = "clmv";
-          AppleShowAllExtensions = true;
-        };
-
-        screencapture.location = "~/Pictures/screenshots";
-
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+        NSGlobalDomain.InitialKeyRepeat = 15;
+        NSGlobalDomain.ApplePressAndHoldEnabled = false;
         loginwindow.GuestEnabled = false;
+        finder.FXPreferredViewStyle = "clmv";
+        finder.AppleShowAllExtensions = true;
+        screencapture.location = "~/Pictures/screenshots";
       };
 
-      programs.fish.enable = true;
-
-      users.users.ciiruu = {
-        home = "/Users/ciiruu";
-        shell = pkgs.fish;
-      };
-
-      environment.shells = with pkgs; [ fish ];
-
+      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+      users.users.ciiruu = {
+          home = "/Users/ciiruu";
+          shell = pkgs.fish;
+      };
+      environment.shells = with pkgs; [
+        fish
+      ];
+
+      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
       system.stateVersion = 5;
 
-      nixpkgs.hostPlatform = system;
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
-
-  in {
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#mac
     darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
-          home-manager.backupFileExtension = "backup";
-
-          home-manager.users.ciiruu =
-            import ./home.nix;
+          home-manager.users.indra = import ./home.nix;
         }
       ];
     };
